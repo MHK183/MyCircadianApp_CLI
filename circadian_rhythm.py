e1406b798c876d63fc829c55cc1a52a73188ae3e
Kim Min Hyeok <4438mh@naver.com>
2023-10-13 14:00:33 +0900

    version 0.0.2

--- circadian_rhythm.py
+++ circadian_rhythm.py
@@ -9,39 +9,64 @@ class CircadianRhythm:
         self.year = date[:4]
         self.month = date[4:6]
         self.day = date[6:]
-        self.first_meal_time = None
-        self.last_meal_time = None
-        self.sleep_duration = None
-        self.wellness_changes = None
+        self.first_meal_time = ''
+        self.last_meal_time = ''
+        self.sleep_duration = ''
+        self.wellness_changes = ''
+
+        self.dir_path = f'data/{self.year}'
+        self.file_path = f'{self.dir_path}/{self.month}.csv'
 
     def _menu(self):
-        self.first_meal_time = input("첫 음식 섭취 시간: ")
-        self.last_meal_time = input("마지막 음식 섭취 시간: ")
-        self.sleep_duration = input("수면 시간: ")
-        self.wellness_changes = input("건강, 기운, 기분상에 주목할 만한 변화: ")
+        data = self._data_reader()
+        if data:
+            for line in data[1:]:
+                if line[0] == self.day:
+                    self.first_meal_time = line[1]
+                    self.last_meal_time = line[2]
+                    self.sleep_duration = line[3]
+                    self.wellness_changes = line[4]
+                    break
+
+        if not self.first_meal_time:
+            self.first_meal_time = input("첫 음식 섭취 시간: ")
+        if not self.last_meal_time:
+            self.last_meal_time = input("마지막 음식 섭취 시간: ")
+        if not self.sleep_duration:
+            self.sleep_duration = input("수면 시간: ")
+        if not self.wellness_changes:
+            self.wellness_changes = input("건강, 기운, 기분상에 주목할 만한 변화: ")
+
+        print(f"first meal time: {self.first_meal_time}")
+        print(f"last meal time: {self.last_meal_time}")
+        print(f"sleep_duration: {self.sleep_duration}")
+        print(f"wellness_changes: {self.wellness_changes}")
+
+    def _data_reader(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r", encoding='utf-8') as file:
+                return list(csv.reader(file))
+        else:
+            return False
 
     def insert(self):
         self._menu()
         self._save()
 
     def delete(self):
-        self.first_meal_time = None
-        self.last_meal_time = None
-        self.sleep_duration = None
-        self.wellness_changes = None
+        self.first_meal_time = ''
+        self.last_meal_time = ''
+        self.sleep_duration = ''
+        self.wellness_changes = ''
         self._save()
 
     def _save(self):
 
-        dir_path = f'data/{self.year}'
-        file_path = f'{dir_path}/{self.month}.csv'
-        if not os.path.exists(dir_path):
-            os.mkdir(dir_path)
+        if not os.path.exists(self.dir_path):
+            os.mkdir(self.dir_path)
 
-        if os.path.exists(file_path):
-            with open(file_path, "r", encoding='utf-8') as file:
-                data = list(csv.reader(file))
-
+        data = self._data_reader()
+        if data:
             for line in data[1:]:
                 if line[0] == self.day:
                     line[1] = self.first_meal_time
@@ -50,10 +75,9 @@ class CircadianRhythm:
                     line[4] = self.wellness_changes
                     break
 
-            with open(file_path, 'w', newline='', encoding='utf-8') as file:
+            with open(self.file_path, 'w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerows(data)
-
         else:
             header_list = ['날짜', '첫 음식 섭취 시간', '마지막 음식 섭취 시간', '수면 시간', '건강, 기운, 기분 상에 주목할 만한 변화']
             initial_data = [header_list]
@@ -63,11 +87,11 @@ class CircadianRhythm:
             for day in range(1, last_day + 1):
                 day = f'{day:02}'
                 if day == self.day:
-                    initial_data.append([self.day, self.first_meal_time, self.last_meal_time, self.sleep_duration,
-                                         self.wellness_changes])
+                    initial_data.append([self.day, self.first_meal_time, self.last_meal_time,
+                                         self.sleep_duration, self.wellness_changes])
                 else:
-                    initial_data.append([day, None, None, None, None])
+                    initial_data.append([day, '', '', '', ''])
 
-            with open(file_path, 'w', newline='', encoding='utf-8') as file:
+            with open(self.file_path, 'w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerows(initial_data)
